6c3c751b7a4d3c6b9bb2f61d59e1df46a01120a3
Yuanqing Zhao <yuanqingz@nvidia.com>
2021-06-18 17:02:16 +0800

    remove duplicated code

--- trainer/run.py
+++ trainer/run.py
@@ -22,52 +22,11 @@ import dllogger
 import horovod.tensorflow as hvd
 import multiprocessing
 import numpy as np
-import portpicker
 import tensorflow as tf
 from data.outbrain.features import DISPLAY_ID_COLUMN
 from tensorflow.python.keras import backend as K
 from trainer.utils.arguments import MODE_HOROVOD
 from trainer.utils.schedulers import get_schedule
-
-
-def create_in_process_cluster(num_workers, num_ps):
-  """Creates and starts local servers and returns the cluster_resolver."""
-  worker_ports = [portpicker.pick_unused_port() for _ in range(num_workers)]
-  ps_ports = [portpicker.pick_unused_port() for _ in range(num_ps)]
-
-  cluster_dict = {}
-  cluster_dict["worker"] = ["localhost:%s" % port for port in worker_ports]
-  if num_ps > 0:
-    cluster_dict["ps"] = ["localhost:%s" % port for port in ps_ports]
-
-  cluster_spec = tf.train.ClusterSpec(cluster_dict)
-
-  # Workers need some inter_ops threads to work properly.
-  worker_config = tf.compat.v1.ConfigProto()
-  if multiprocessing.cpu_count() < num_workers + 1:
-    worker_config.inter_op_parallelism_threads = num_workers + 1
-
-  for i in range(num_workers):
-    tf.distribute.Server(
-        cluster_spec, job_name="worker", task_index=i, config=worker_config,
-        protocol="grpc")
-
-  for i in range(num_ps):
-    tf.distribute.Server(
-        cluster_spec, job_name="ps", task_index=i, protocol="grpc")
-
-  cluster_resolver = tf.distribute.cluster_resolver.SimpleClusterResolver(
-      cluster_spec, rpc_layer="grpc")
-  return cluster_resolver
-
-
-# Set the environment variable to allow reporting worker and ps failure to the
-# coordinator. This is a workaround and won't be necessary in the future.
-os.environ["GRPC_FAIL_FAST"] = "use_caller"
-
-NUM_WORKERS = 3
-NUM_PS = 2
-cluster_resolver = create_in_process_cluster(NUM_WORKERS, NUM_PS)
 
 
 def train(args, model, config):
